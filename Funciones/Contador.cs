c5a07bd7a4d6e2c1d2c7f8777ee15662efd48e45
Vicky <vickymap@gmail.com>
2021-05-24 19:12:30 -0300

    funciones2

---
+++ Funciones/Contador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Contador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            Contador(i);
+
+            static void Contador(int i) {
+                int a, b, c;
+                for (i = 1; i <= 10; i++) {
+                    Console.WriteLine(i);
+                }
+                for (i = 10; i >= 1; i=i-2)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("Inicio");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Fim");
+                b=Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Passo");
+                c= Convert.ToInt32(Console.ReadLine());
+
+                for (i=a;i<=b;i=i+c) {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ Funciones/SomatoriaePar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SomatoriaePar
+{
+    class SomatoriaePar
+    {
+        static void Main(string[] args)
+        {
+            int[] Numeros = new int[5];
+            int i= 0;
+            int Soma = 0;
+
+            Sorteia(Numeros);
+            Somatoria(Soma, Numeros);
+            
+
+         
+        }
+        public static int Sorteia(int[] Numeros)
+        {
+            int i = 0;
+           
+            for (i=0; i<5;i++) { 
+            Random random = new Random();
+            Numeros[i] = random.Next(0, 100);
+                Console.WriteLine( Numeros[i]);
+            }
+            return 1;
+
+        }
+
+        public static int Somatoria(int Soma, int[] Numeros)
+        {
+            int i = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                if (Numeros[i] % 2 == 0) {
+                    Soma = Soma + Numeros[i];
+                }
+                
+            }
+
+            Console.WriteLine("A somatoria dos números pares é:"+Soma); 
+            return Soma;
+
+        }
+    }
+}
