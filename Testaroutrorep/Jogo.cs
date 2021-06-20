6b07f1a4d4a295db8736afc8f86af0fbd6748240
VicMayte1986 <virginia.m.sanchez@exxonmobil.com>
2021-06-20 16:27:29 -0300

    testar_novo

---
+++ Testaroutrorep/Jogo.cs
@@ -0,0 +1,20 @@
+﻿using Jokenpo;
+using System;
+
+namespace Jogador_JokenpoPOO
+{
+    class Jogo
+    {
+        static void Main(string[] args)
+        {
+            Jogador jogador1, jogador2;
+
+            jogador1 = ControleJogo.CadastroJogador();
+            jogador2 = ControleJogo.CadastroJogador();
+
+            ControleJogo.Vencedor(jogador1, jogador2);
+        }
+
+       
+    }
+}
